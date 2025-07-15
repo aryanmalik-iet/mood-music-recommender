0df510d215f1d51e6ff6b268d7e4545acaeb9196
Aryan Malik <aryanmalik.sbps@gmail.com>
2025-07-15 12:46:23 +0530

    Update index.js

--- static/animation/index.js
+++ static/animation/index.js
@@ -1,1 +1,15 @@
+// Central export for mood animation modules
+import { createContinuousBubbleEffect } from './happy.js';
+import { createContinuousRainEffect } from './sad.js';
+import { createContinuousHeartEffect } from './romantic.js';
+import { createContinuousLightningEffect } from './energetic.js';
+import { createContinuousWaveEffect } from './calm.js';
+
+export const moodAnimations = {
+    happy: createContinuousBubbleEffect,
+    sad: createContinuousRainEffect,
+    romantic: createContinuousHeartEffect,
+    energetic: createContinuousLightningEffect,
+    calm: createContinuousWaveEffect
+};
 
